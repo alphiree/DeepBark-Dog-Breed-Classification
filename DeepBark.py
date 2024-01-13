07207bedbff2bca368bc1f7b41491bdbcaefa45d
ryanlupague <ryan.lupague@indebted.co>
2024-01-13 14:11:05 +0800

    Refactor main script

--- DeepBark.py
+++ DeepBark.py
@@ -1,100 +1,35 @@
-from deeplearning_packages import *
-from deeplearning_functions import *
+import os
 
-model = load_model('saved-models\\mobilenetV2.h5')
+from keras.models import load_model
 
-print('')
-print('DeepBark: Dog Breed Classification by: Alphire :)')
-print('')
+from config.base import Config
+from deeplearning_functions import (
+    multiclass_predict_from_image_with_comparison,
+    read_image,
+)
+
+model = load_model(
+    os.path.join(f"{os.getcwd()}", f"{Config.model_folder}", "mobilenetV2.h5")
+)
+
+print("")
+print("DeepBark: Dog Breed Classification by: Alphire :)")
+print("")
 # Get input string from user
 input_string = input("Enter the file path: ")
-
 # Add another backslash to the string
 new_string = input_string.replace("\\", "\\\\")
-
 # Remove double quotes from the string
-new_string = new_string.replace('"', '')
-
-class_names = ['Afghan',
- 'African Wild Dog',
- 'Airedale',
- 'American Hairless',
- 'American Spaniel',
- 'Basenji',
- 'Basset',
- 'Beagle',
- 'Bearded Collie',
- 'Bermaise',
- 'Bichon Frise',
- 'Blenheim',
- 'Bloodhound',
- 'Bluetick',
- 'Border Collie',
- 'Borzoi',
- 'Boston Terrier',
- 'Boxer',
- 'Bull Mastiff',
- 'Bull Terrier',
- 'Bulldog',
- 'Cairn',
- 'Chihuahua',
- 'Chinese Crested',
- 'Chow',
- 'Clumber',
- 'Cockapoo',
- 'Cocker',
- 'Collie',
- 'Corgi',
- 'Coyote',
- 'Dalmation',
- 'Dhole',
- 'Dingo',
- 'Doberman',
- 'Elk Hound',
- 'French Bulldog',
- 'German Sheperd',
- 'Golden Retriever',
- 'Great Dane',
- 'Great Perenees',
- 'Greyhound',
- 'Groenendael',
- 'Irish Spaniel',
- 'Irish Wolfhound',
- 'Japanese Spaniel',
- 'Komondor',
- 'Labradoodle',
- 'Labrador',
- 'Lhasa',
- 'Malinois',
- 'Maltese',
- 'Mex Hairless',
- 'Newfoundland',
- 'Pekinese',
- 'Pit Bull',
- 'Pomeranian',
- 'Poodle',
- 'Pug',
- 'Rhodesian',
- 'Rottweiler',
- 'Saint Bernard',
- 'Schnauzer',
- 'Scotch Terrier',
- 'Shar_Pei',
- 'Shiba Inu',
- 'Shih-Tzu',
- 'Siberian Husky',
- 'Vizsla',
- 'Yorkie']
+new_string = new_string.replace('"', "")
 
 image_read = read_image(new_string)
 
-## Setting the path for image loading
-train_path = 'train'
 
-
-multiclass_predict_from_image_with_comparison(model,
-image = image_read,
-image_size = (224,224),
-class_names = class_names,
-comparison_path = train_path,
-picture_title = 'Dog Breed Classification')
+multiclass_predict_from_image_with_comparison(
+    model,
+    image=image_read,
+    image_size=(224, 224),
+    class_names=Config.class_names,
+    comparison_path="train",
+    picture_title="Dog Breed Classification",
+)
